836ec423a6004f81fdd59bc074963590736f9cd2
bucux <ubucux@gmail.com>
2024-03-20 15:40:50 +0100

    ultimate

--- src/comps/header.tsx
+++ src/comps/header.tsx
@@ -4,7 +4,10 @@ import './css/header.css'
 export default function Header({stop} : {stop: ()=>void}) {
   return (
     <div className='header-cont0'>
-      <p>Ready To Go</p>
+      <div className='header-cont1'>
+        <img src='imgFusee2.png' alt='fusée' style={{width: '100px', height: '100px'}}/>
+        <p>Ready To Go</p>
+      </div>
       <button className='stop' onClick={stop}>STOP</button>
     </div>
   )
